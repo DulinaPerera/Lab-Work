9fe25ec30682df3cd4d1e85fe98b7b0897709270
Dulina Perera <109563701+DulinaPerera@users.noreply.github.com>
2023-04-02 13:56:50 +0530

    Update runningMedian.cpp

--- Lab-5/runningMedian.cpp
+++ Lab-5/runningMedian.cpp
@@ -7,32 +7,26 @@ int main() {
     vector<int> integerVector;
     int element;
 	
-    while (cin >> element) // Read inputs until end-of-file (EOF) is reached.
-    {
+    while (cin >> element) // Read inputs until end-of-file (EOF) is reached. {
         integerVector.push_back(element);
 		
-	    // Since 'integerVector' was sorted before the introduction of 'element', use insertion sort to place 'element' in its correct position. 
-        for (int i = integerVector.size() - 2; i >= 0; i--) 
-		{
-            if (integerVector[i] > integerVector[i + 1]) 
-            {
+	// Since 'integerVector' was sorted before the introduction of 'element', use insertion sort to place 'element' in its correct position. 
+        for (int i = integerVector.size() - 2; i >= 0; i--) {
+            if (integerVector[i] > integerVector[i + 1]) {
                 swap(integerVector[i], integerVector[i + 1]);
             }
-            else 
-            {
+            else {
                 break;
             }
         }
 		
-	    /* If 'integerVector' has an odd number of elements, the median is the element in the middle. Otherwise it is the average of the two elements in the middle.
+	/* If 'integerVector' has an odd number of elements, the median is the element in the middle. Otherwise it is the average of the two elements in the middle.
          * Since median is either 'x' or 'x.5', calculate & print median as a one decimal point number to STDOUT.
          */        
-        if (integerVector.size() % 2 == 1)  
-		{
+        if (integerVector.size() % 2 == 1) {
             cout << fixed << setprecision(1) << integerVector[integerVector.size() / 2] << ".0\n";
         }
-        else 
-		{
+        else {
             cout << (double)(integerVector[integerVector.size() / 2 - 1] + integerVector[(integerVector.size() / 2)]) / 2 << endl;
         }
     }
