58fbf5648a911fe446a952e2c162b49d9111599a
Dulina Perera <109563701+DulinaPerera@users.noreply.github.com>
2023-05-04 16:31:54 +0530

    Comments
    Commented the heap sort implementation.

--- Lab-8/main.cpp
+++ Lab-8/main.cpp
@@ -13,64 +13,93 @@ void displayArray(int array[], int arraySize) {
     std::cout << "\n";
 }
 
+/**
+ * Performs a sift down operation to maintain the heap property at a given index.
+ *
+ * @param array Array of integers
+ * @param arraySize Size of the array
+ * @param root Index of the root of the subtree which has to be converted to a heap
+ */
 void heapify(int array[], const int arraySize, const std::size_t root) {
     const std::size_t left = 2 * root + 1, right = left + 1;
-    int largest; // Index of the largest amongst "root", "left" & "right"
+    std::size_t largest; // Index of the largest amongst the elements at "root", "left" & "right" indices
 
+    // Determine the largest element amongst the root element, left child element & right child element.
     if (left < arraySize && array[root] < array[left]) {
         largest = left;
     }
     else {
         largest = root;
     }
-
     if (right < arraySize && array[largest] < array[right]) {
         largest = right;
     }
 
+    // If the largest element is not at the root, swap the root element with the largest element.
+    // Heapify the affected subtree.
     if (largest != root) {
         std::swap(array[root], array[largest]);
         heapify(array, arraySize, largest);
     }
 }
-void buildHeap(int arr[], const int n) {
-    for (int i = (n / 2) - 1; i >= 0; i--) {
-        heapify(arr, n, i);
+/**
+ * Builds a max heap from a given array of integers.
+ *
+ * @param array Array of integers
+ * @param arraySize Size of the array
+ */
+void buildHeap(int array[], const int arraySize) {
+    // Heapify all non-leaf nodes in the array.
+    for (int i = (arraySize / 2) - 1; i >= 0; i--) {
+        heapify(array, arraySize, i);
     }
 }
 
-void heapSort(int arr[], int n) {
-    buildHeap(arr, n);
+/**
+ * Sorts a given array of integers using the heap sort algorithm.
+ *
+ * @param array Array to be sorted
+ * @param arraySize Size of the array
+*/
+void heapSort(int array[], int arraySize) {
+    // Build a max heap from the array.
+    buildHeap(array, arraySize);
 
-    for (int i = n - 1; i >= 1; i--) {
-        std::swap(arr[0], arr[i]);
+    // Extract elements one by one from the heap.
+    // Heapify the remaining elements.
+    for (int i = arraySize - 1; i >= 1; i--) {
+        std::swap(array[0], array[i]);
 
-        heapify(arr, i, 0);
+        heapify(array, i, 0);
     }
 }
 
 int main() {
-    int* heap = nullptr;
-    int heapSize = 0;
+    int* array = nullptr;
+    int arraySize = 0;
     int userInput;
 
+    // Prompt the user to enter integers seperated by whitespaces and read them until an invalid input is provided.
     std::cout << "Enter integers: ";
     while (std::cin >> userInput) {
-        int* temp = new int[heapSize + 1];
+        int* temp = new int[arraySize + 1];
 
-        for (int i = 0; i < heapSize; i++) {
-            temp[i] = heap[i];
+        for (int i = 0; i < arraySize; i++) {
+            temp[i] = array[i];
         }
-        temp[heapSize++] = userInput;
-        delete heap;
-        heap = temp;
+        temp[arraySize++] = userInput;
+        delete array;
+        array = temp;
     }
 
+    // Display the array before sorting.
     std::cout << "Input array: " << std::endl;
-    displayArray(heap,heapSize);
+    displayArray(array, arraySize);
 
-    heapSort(heap, heapSize);
+    // Sort the array using heap sort algorithm.
+    heapSort(array, arraySize);
 
+    // Display the sorted array.
     std::cout << "Sorted array: "<< std::endl;
-    displayArray(heap, heapSize);
+    displayArray(array, arraySize);
 }
